898b3404ea4ed5417f202dbfbf3af22f2e2a4b33
Derrick <derrick.strebel@gmail.com>
2020-10-25 23:18:13 -0500

    Upgraded Qt5 to 5.15.1

--- files/qt5.sh
+++
@@ -1,9 +0,0 @@
-# Begin /etc/profile.d/qt5.sh
-
-export QT5DIR=/opt/qt5
-
-export PATH=$PATH:$QT5DIR/bin
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$QT5DIR/lib/pkgconfig
-
-
-# End /etc/profile.d/qt5.sh
